31300c02f8f2561eb5fc6f06d95971bfc9a99177
ayuka-supra <ayukasupra@gmail.com>
2025-07-27 16:44:24 +1000

    added form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello world");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Generating recipe for you..",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
