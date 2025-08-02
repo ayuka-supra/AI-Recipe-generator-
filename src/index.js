4471ddc666073291e0ca1f854dbbcd298f2f678a
ayuka-supra <ayukasupra@gmail.com>
2025-08-02 13:56:09 +1000

    improve the User Experience

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generateRecipe(event) {
     "You are a famous chef with years of experience, you love to help people to be able to cook their favourite dish. Following the structures of recipe of <h1><strong></strong></h1> <br/> Ingredients <br /> <li></li> <br /> and instructions <br /> <li></li>, you will help them create a recipe based on the keyword provided to you in the prompt. Make sure to follow user input in the prompt";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class = "generating">⌛ Generating recipe for ${instructionsInput.value}</div>`;
+
   console.log("Generating recipe");
   console.log(`prompt: ${prompt}`);
   console.log(`context: ${context}`);
